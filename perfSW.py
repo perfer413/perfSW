6bea9fac8837d7b52aab753b2f2e7a74d1b1a01b
perfer413 <aleksei.prok10@gmail.com>
2026-01-04 22:36:13 +0300

    v.0.0.0.1

--- perfSW.py
+++ perfSW.py
@@ -1,2 +1,15 @@
+#source .env/bin/activate
 import cv2
 import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+
+
+while True:
+    ret, img = cap.read()
+    cv2.imshow("hand", img)
+    if cv2.waitKey(10) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows() 
